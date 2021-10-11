d44a233fa521c7e4f489df5c2c5ada77cd305504
yl301114 <yl301114@gmail.com>
2021-10-12 05:07:17 +0800

    new

---
+++ public/js/loading.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("header").load("header.html");
+    $(".alert").load("alert.html");
+    $(".footer").load("classfooter.html");
+    $("footer").load("footer.html");
+});
